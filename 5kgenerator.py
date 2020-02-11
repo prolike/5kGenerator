d79701441bc62d4d7138121a7c7f2c359c8868e2
sygdrome <sygdrom@hotmail.com>
2020-02-11 16:22:01 +0100

    close #15 find and implement a solution for the html proofer

--- 5kgenerator.py
+++ 5kgenerator.py
@@ -29,34 +29,41 @@ path = cfg['repo']['path']
 email = cfg['repo']['email']
 citoken = cfg['repo']['citoken']
 ciEnvName = cfg['repo']['cienvname']
+htmlproofer = cfg['repo']['htmlproofer']
+theme = cfg['repo']['theme']
+
+
 
 
 #Create repos
 source ="curl -i -H 'Authorization: token " + key + " ' -d '{ \"name\":\"" + domain + "\", \"private\": " + sourcePrivate + "  }' https://api.github.com/user/repos "
-#stage ="curl -i -H 'Authorization: token " + key + " ' -d '{ \"name\":\"stage." + domain + "\", \"private\": " + stagePrivate + " }' https://api.github.com/user/repos "
-#prod ="curl -i -H 'Authorization: token " + key + " ' -d '{ \"name\":\"www." + domain + "\", \"private\": " + prodPrivate + " }' https://api.github.com/user/repos "
+stage ="curl -i -H 'Authorization: token " + key + " ' -d '{ \"name\":\"stage." + domain + "\", \"private\": " + stagePrivate + " }' https://api.github.com/user/repos "
+prod ="curl -i -H 'Authorization: token " + key + " ' -d '{ \"name\":\"www." + domain + "\", \"private\": " + prodPrivate + " }' https://api.github.com/user/repos "
 
 os.system(source)
-#os.system(stage)
-#os.system(prod)
+os.system(stage)
+os.system(prod)
 
 
 #Clone repos and where to place them
 path  = ""+ path +""
 repo = "git clone git@github.com:" + user + "/" + domain + ""
 temp = "git clone git@github.com:" + templateUser + "/" + templateName + ".git"
-them = "git clone git@github.com:" + themeUser + "/" + themeName + ".git"
+if theme:
+    them = "git clone git@github.com:" + themeUser + "/" + themeName + ".git"
 
 #os.system("sshpass -p your_password ssh user_name@your_localhost")
 os.chdir(path) # Specifying the path where the cloned project needs to be copied
 os.system(repo) # Cloning the source repo
 os.system(temp) # Cloning template repo
-os.system(them) # Cloning theme repo
+if theme:
+    os.system(them) # Cloning theme repo
 
 
 #Directories for template, theme and source repo
 source = "" + path + "/" + templateName + "/"
-src_dir = "" + path + "/" + themeName + "/"
+if theme:
+    src_dir = "" + path + "/" + themeName + "/"
 dest_dir = "" + path + "/" + domain +"/"
 
 
@@ -68,24 +75,26 @@ for f in files:
         continue
     shutil.move(source+f, dest_dir)
 
-print('merging theme')
-#Move files from template into source folder
-def move_over(src_dir, dest_dir):
-    fileList = os.listdir(src_dir)
-    for i in fileList:
-        if i == ".git" or i.endswith(".gemspec") or i == "Gemfile":
-            continue
-        src = os.path.join(src_dir, i)
-        dest = os.path.join(dest_dir, i)
-        if os.path.exists(dest):
-            if os.path.isdir(dest):
-                move_over(src, dest)
-                continue
-            else:
-                os.remove(dest)
-        shutil.move(src, dest_dir)
+if theme:
 
-move_over(src_dir, dest_dir)
+    print('merging theme')
+    #Move files from template into source folder
+    def move_over(src_dir, dest_dir):
+        fileList = os.listdir(src_dir)
+        for i in fileList:
+            if i == ".git" or i.endswith(".gemspec") or i == "Gemfile":
+                continue
+            src = os.path.join(src_dir, i)
+            dest = os.path.join(dest_dir, i)
+            if os.path.exists(dest):
+                if os.path.isdir(dest):
+                    move_over(src, dest)
+                    continue
+                else:
+                    os.remove(dest)
+            shutil.move(src, dest_dir)
+
+    move_over(src_dir, dest_dir)
 
 
 #Change CNAME files
@@ -118,6 +127,11 @@ def ciConfig():
         pattern = re.compile(r'(?<=stage-deploy:(\n.*){0,7}PLAY_USER_EMAIL:\s+)\S+')
         filedata = re.sub(pattern, f'{email}', filedata)
 
+        if htmlproofer != True:
+            print('skipping html proofer')
+            pattern = re.compile(r'(?<=deliver:.*(\n.*){0,50}requires:.*(\n.*){0,7}\s+)- html-proofer')
+            filedata = re.sub(pattern, f'#- html-proofer', filedata)
+
 
         pattern = re.compile(r'(?<=prod-deploy:(\n.*){0,7}PLAY_TARGET_GH_REPO:\s+)\S+')
         filedata = re.sub(pattern, f'{user}/www.{domain}', filedata)
@@ -130,6 +144,7 @@ def ciConfig():
 
         pattern = re.compile(r'(?<=jekyll-build:(\n.*){0,7}image:\s+)\S+')
         filedata = re.sub(pattern, f'carolineolivia94/jekyll-plus-plus', filedata)
+
 
     with open("" + path + "/" "" + domain + "/.circleci/config.yml", 'w') as file:
         file.write(filedata)
@@ -154,23 +169,24 @@ def git_push():
 
 git_push()
 
-# def followci():
-#     url = f"https://circleci.com/api/v1.1/project/github/{user}/{domain}/follow?circle-token={citoken}"
-#     resp = requests.post(url)
-#     print('following project')
-
-# followci()
-
-# def addci():
-#     if citoken != "":
-#         url = f"https://circleci.com/api/v1.1/project/github/{user}/{domain}/envvar?circle-token={citoken}"
-#         data = {
-#         "name": ciEnvName,
-#         "value": key
-#         }
-#         resp = requests.post(url, data=data)
-#         print('project added to circle ci')
-#     else:
-#         print('skipped adding token to circle ci, please add manually')
-
-# addci()
+if circle = True:
+    def followci():
+        url = f"https://circleci.com/api/v1.1/project/github/{user}/{domain}/follow?circle-token={citoken}"
+        resp = requests.post(url)
+        print('following project')
+    
+    followci()
+    
+    def addci():
+        if citoken != "":
+            url = f"https://circleci.com/api/v1.1/project/github/{user}/{domain}/envvar?circle-token={citoken}"
+            data = {
+            "name": ciEnvName,
+            "value": key
+            }
+            resp = requests.post(url, data=data)
+            print('project added to circle ci')
+        else:
+            print('skipped adding token to circle ci, please add manually')
+    
+    addci()
